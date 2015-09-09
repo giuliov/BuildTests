a1783eb06c5c9e609a405f7bb7bc6063ef77bf93
Giulio Vian <giuliovdev@gmail.com>
2015-09-09 19:13:12 +0200

    comment

--- DummyConsoleApp1/Program.cs
+++ DummyConsoleApp1/Program.cs
@@ -10,6 +10,7 @@ namespace DummyConsoleApp1
     {
         static void Main(string[] args)
         {
+            // a comment
             Console.WriteLine(GetHeader());
             Console.WriteLine();
             Console.WriteLine("Hello world!");
