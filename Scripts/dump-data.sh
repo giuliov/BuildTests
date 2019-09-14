8bcde61f9f1d99edb1554ae5f6ef87c22dbbb052
Giulio Vian <giuliovdev@hotmail.com>
2019-09-14 16:23:36 +0100

    last attempt

--- Scripts/dump-data.sh
+++ Scripts/dump-data.sh
@@ -3,7 +3,13 @@
 if [ "$1" == "" ]; then
     echo "Path to data file required"
 else
-    jq -r '.' $1
-fi
+    export FNAME=$1
+    
+    F1=$(jq -r '.field1?' $FNAME)
+    F2=$(jq -r '.field2?' $FNAME)
+    F3=$(jq -r '.field3?' $FNAME)
+    
+    echo "Found $F1,$F2,$F3."
 
-rm $1
+    rm $FNAME
+fi
