4b0881e4461b97288194df8d3e78e07be128b84c
Giulio Vian <giuliovdev@hotmail.com>
2020-04-04 16:06:36 +0100

    remove dups attributes
    AssemblyCompany
    AssemblyConfiguration
    AssemblyProduct
    AssemblyTitle

--- DummyConsoleApp3/Properties/AssemblyInfo.cs
+++ DummyConsoleApp3/Properties/AssemblyInfo.cs
@@ -5,14 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DummyConsoleApp1")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("The Company")]
-[assembly: AssemblyProduct("DummyConsoleApp1")]
 [assembly: AssemblyCopyright("Copyright © Giulio Vian")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
