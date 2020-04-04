e0346830300dc35c78801dbe23ddbf5ad437586e
Giulio Vian <giuliovdev@hotmail.com>
2020-04-04 15:58:39 +0100

    explore gitversion action and dotnet core build

---
+++ DummyConsoleApp3/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DummyConsoleApp1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("The Company")]
+[assembly: AssemblyProduct("DummyConsoleApp1")]
+[assembly: AssemblyCopyright("Copyright © Giulio Vian")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b9809f1a-b3a7-4e3a-8d79-019c773fd7f4")]
